c0ed70af89f98b572e961504814ebf59fd8d45b8
Sunny-The7ank <firstru7h@gmail.com>
2020-09-11 20:59:02 -0400

    Write notes

--- 1_2_1-debug/src/main.rs
+++ 1_2_1-debug/src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
-}
+/*
+Notes:
+All types that want to use std::fmt formatting traits require an implementation 
+to be printable.  Auto implementations are only provided for types in the std
+library.  All others mus be manually implemented somehow
+
+
+
+// This structure cannot be printed either with `fmt::Display` or
+// with `fmt::Debug`.
+struct UnPrintable(i32);
+
+// The `derive` attribute automatically creates the implementation
+// required to make this `struct` printable with `fmt::Debug`.
+#[derive(Debug)]
+struct DebugPrintable(i32);
+
+All std library types are automatically printable with {:?} too
+*/
+
