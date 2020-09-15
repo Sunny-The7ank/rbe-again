e84b4d29bf17bb3beb5f3831ce7cdb48347dfb8d
Sunny-The7ank <firstru7h@gmail.com>
2020-09-14 20:55:33 -0400

    added notes

--- 1_2_3-formatting/src/main.rs
+++ 1_2_3-formatting/src/main.rs
@@ -49,11 +49,30 @@ impl Display for Color {
     }
 }
 
+// This is the more correct response
 impl fmt::UpperHex for Color {
     fn fmt(&self, f: &mut Formatter) -> fmt::Result {
         write!(f, "{:02X}{:02X}{:02X}", self.red, self.green, self.blue)
     }
 }
+
+// Solution seen elsewhere
+/*
+impl Display for Color {
+    // `f` is a buffer, this method must write the formatted string into it
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        // RGB (128, 255, 9) 0x80FF5A  0x{:>02X}{:>02X}{:>02X}
+        // I am not very happy with this solution. I think this can be done in a better way.
+        write!(f, "RGB ({0}, {1}, {2}) 0x{3}{4}{5}",
+               self.red, self.green, self.blue,
+               format!("{:01$x}", self.red, 2),
+               format!("{:01$x}", self.green, 2),
+               format!("{:01$x}", self.blue, 2))
+    }
+}
+
+I don't like this because {:X} can just be used to call upper hex
+*/
 
 fn main() {
     for city in [
