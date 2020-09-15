311b4bff24fd3c411358b9b34ab35cb349a3c837
Sunny-The7ank <firstru7h@gmail.com>
2020-09-14 21:09:31 -0400

    Notes added

--- 2_0-primitives/src/main.rs
+++ 2_0-primitives/src/main.rs
@@ -1,3 +1,29 @@
+/*
+####### NOTES #######
+
+Rust provides access to a wide variety of primitives. EX: 
+
+Scalar types
+    Signed integers: i8, i16, i32, i64, i128, and isize(pointer size)
+    Unsigned integers: u8, u16, u32, u64, u128, and usize(pointer size)
+    Floating point: f32, f64
+    Unicode scalar values: char ('a','α', and '∞') 4 bytes each
+    Boolean: bool either true or false
+    and the unit type (), whose only value is an empty tuple: ()
+
+Despite the value of a unit type being a tuple, it is not considered a compound type
+because it doesn't contain multiple values. 
+
+Compound types
+    arrays like [1, 2, 3]
+    tuples like (1, true)
+
+Variables can always be type annotated. Numbers may additionally be annotated via a 
+suffix or by default. Integers default to i32 and floats to f64. Note that Rust can also
+infer types from context
+
+*/
+
 fn main() {
     // Variables can be type annotated.
     let logical: bool = true;
@@ -18,7 +44,7 @@ fn main() {
     mutable = 21;
     
     // Error! The type of a variable can't be changed.
-    mutable = true;
+    //mutable = true;
     
     // Variables can be overwritten with shadowing.
     let mutable = true;
